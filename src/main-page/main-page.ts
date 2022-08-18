985943646646eb7f5e3dabb616f59b990c86d6f6
Olga <olga.dashuk21@gmail.com>
2022-08-18 01:54:57 -0400

    feat: design and render main page

--- src/main-page/main-page.ts
+++ src/main-page/main-page.ts
@@ -2,10 +2,83 @@ import renderHeader from '../components/header';
 import renderSidebar from '../components/sidebar';
 import renderFooter from '../components/footer';
 import createElement from '../helpers';
+import './main-page.scss';
+import strings, { creatorsDescriptions, mainDescriptions } from '../constants';
+
+const createMainMessage = (img: string, message: string) => {
+  const mainMessage = createElement('div', ['main__message']);
+  const messageText = createElement('div', 'main__message-text');
+  messageText.textContent = message;
+  const messageImg = createElement('img', 'main__message-img') as HTMLImageElement;
+  messageImg.src = img;
+  mainMessage.append(messageImg, messageText);
+  return mainMessage;
+};
+
+const renderMainMessages = () => (
+  mainDescriptions.map(({ img, message }) => createMainMessage(img, message))
+);
+
+const createCreatorDescription = (
+  img: string,
+  name: string,
+  role: string,
+  githubLink: string,
+  description: string,
+) => {
+  const creatorDescription = createElement('div', ['creators__item']);
+  const creatorText = createElement('div', 'creator-text');
+  creatorText.textContent = description;
+  const creatorName = createElement('div', 'creator-name');
+  creatorName.textContent = name;
+  const creatorRole = createElement('div', 'creator-role');
+  creatorRole.textContent = role;
+  const creatorImg = createElement('img', 'creator-img') as HTMLImageElement;
+  creatorImg.src = img;
+  const creatorLink = createElement('a', 'creator-link') as HTMLAnchorElement;
+  creatorLink.href = githubLink;
+  creatorDescription.append(creatorImg, creatorName, creatorRole, creatorText, creatorLink);
+  return creatorDescription;
+};
+
+const renderCreatorDescriptions = () => (
+  creatorsDescriptions.map(({
+    img, name, role, githubLink, description,
+  }) => (
+    createCreatorDescription(img, name, role, githubLink, description)
+  ))
+);
 
 const renderMainContent = () => {
-  const mainContent = createElement('div', 'main__wrapper');
-  return mainContent;
+  const mainPageContent = createElement('div', 'main__wrapper');
+
+  const mainContent = createElement('div', 'main-content__wrapper');
+
+  const imageWrapper = createElement('div', 'image__wrapper');
+  const mainLogo = createElement('h1', 'main__logo');
+  mainLogo.textContent = 'RS Lang';
+  const mainBtn = createElement('button', ['main__button', 'btn', 'btn-dark']) as HTMLButtonElement;
+  mainBtn.innerText = strings.registration;
+  imageWrapper.append(mainLogo, mainBtn);
+
+  const textWrapper = createElement('div', 'text__wrapper');
+  const mainMessages = renderMainMessages();
+  textWrapper.append(...mainMessages);
+
+  mainContent.append(imageWrapper, textWrapper);
+
+  const creatorsContent = createElement('div', 'creators-content__wrapper');
+  const creatorsWrapper = createElement('div', 'creators-info__wrapper');
+  const creatorsInfo = renderCreatorDescriptions();
+  const creatorsTitle = createElement('h2', 'creators-content__title');
+  creatorsTitle.textContent = strings.creatorsTitle;
+  creatorsWrapper.append(...creatorsInfo);
+
+  creatorsContent.append(creatorsTitle, creatorsWrapper);
+
+  mainPageContent.append(mainContent, creatorsContent);
+
+  return mainPageContent;
 };
 
 const renderMainPage = () => {
