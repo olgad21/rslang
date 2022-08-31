d4d8ca0a07dd40630cf889cbf889d6a2f5c7ddb0
Alexei <marusovalexei@gmail.com>
2022-09-01 01:24:02 +0300

    fix: fix code style

---
+++ src/games/sprint/timer/timer.ts
@@ -0,0 +1,19 @@
+import createModal from '../../modal/modal';
+
+const getNewValue = (place: HTMLElement) => {
+  const prevValue: number = parseInt(<string>place.textContent, 10);
+  let nextValue: number = prevValue - 1;
+
+  const timer = setInterval(() => {
+    if (nextValue === 0) {
+      clearInterval(timer);
+      createModal();
+    } else {
+      place.textContent = '';
+      place.textContent = String(<number>nextValue);
+      nextValue -= 1;
+    }
+  }, 1000);
+};
+
+export default getNewValue;
