5969d60d6aaa53e061082c6915feb8f34f975cef
Alexei <marusovalexei@gmail.com>
2022-09-01 01:20:12 +0300

    feat: add new enums

---
+++ src/games/sprint/sprint-enum.ts
@@ -0,0 +1,23 @@
+const enum Sprint {
+  levl = 'Уровень: ',
+  title = 'Ваш результат: ',
+  timer = '60',
+  btnFalse = 'False',
+  btnTrue = 'True',
+}
+
+const enum Modal {
+  yourResults = 'Ваш результат: ',
+  correctAnswer = 'Правильных ответов: ',
+  incorrectAnswer = 'Не правильных ответов: ',
+  return = 'Повторить',
+  menu = 'Меню',
+}
+
+const enum SprintMenu {
+  title = 'СПРИНТ',
+  description = 'СПРИНТ - игра, где Вы за отведенное время должны сказать, правильно ли программа подобрала перевод для слова на английском языке. Постарайтесь набрать наибольшее количество баллов!',
+  btn = 'играть',
+}
+
+export { Sprint, Modal, SprintMenu };
